c4f033f47e4171d3b9254e4f9fc7b8ddd9d63310
Sehyun Hwang <hwanghyun3@gmail.com>
2022-02-27 17:43:23 +0900

    Echo to stdout

--- secret.fish
+++ secret.fish
@@ -1,25 +1,25 @@
 #!/bin/env fish
 
 #echo $argv
-status dirname
+#status dirname
 set SECRETS (cat (status dirname)/.secret.csv)
 
 for X in $SECRETS
     set LIST (string split ' ' $X)
-    
+
     if contains $LIST[1] $argv
         set -x $LIST[1] $LIST[2]
-        echo Setting $LIST[1]
+        echo Setting $LIST[1] 1>&2
+        echo $LIST[2]
     end
 end
 
 for I in (seq (count $argv))
     set EXE $argv[$I]
-    if which $EXE
+    if which $EXE 1>&2
         $argv[$I..-1]
         exit
     end
 end
 
-echo 'Executable not found'
-
+echo 'Executable not found' 1>&2
