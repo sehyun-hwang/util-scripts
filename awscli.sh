39b0210f216c2b26579ed5aef96d62b1758cc06b
Sehyun Hwang <hwanghyun3@gmail.com>
2022-02-27 17:42:22 +0900

    AWS CLI

--- awscli.sh
+++ awscli.sh
@@ -1,6 +1,7 @@
 set -e
 cd /volatile/src/awscli
 
+pip install -t awscli -U https://github.com/boto/botocore/archive/v2.zip https://github.com/aws/aws-cli/archive/v2.zip
+
 docker create --name aws amazon/aws-cli
-pip3 install -t awscli -U https://github.com/boto/botocore/archive/v2.zip https://github.com/aws/aws-cli/archive/v2.zip
-docker cp aws:/usr/local/aws-cli/v2/2.2.41/dist/awscli/data/ac.index awscli/data/
+docker cp aws:/usr/local/aws-cli/v2/2.4.0/dist/awscli/data/ac.index awscli/data/
