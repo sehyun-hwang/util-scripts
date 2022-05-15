7cd8bf6a1b35d8f68c901ab67b3715a2cae29d90
Sehyun Hwang <hwanghyun3@gmail.com>
2022-05-15 19:23:00 +0900

    aws cli version in install script

--- awscli.sh
+++ awscli.sh
@@ -1,7 +1,10 @@
 set -e
 cd /volatile/src/awscli
 
-pip install -t awscli -U https://github.com/boto/botocore/archive/v2.zip https://github.com/aws/aws-cli/archive/v2.zip
+#pip install -t . -U https://github.com/boto/botocore/archive/v2.zip https://github.com/aws/aws-cli/archive/v2.zip
 
-docker create --name aws amazon/aws-cli
-docker cp aws:/usr/local/aws-cli/v2/2.4.0/dist/awscli/data/ac.index awscli/data/
+[[ `python -m awscli --version` =~ aws-cli/([0-9.]+) ]] && VERSION=${BASH_REMATCH[1]}
+echo $VERSION
+
+docker create --name aws amazon/aws-cli:$VERSION
+docker cp aws:/usr/local/aws-cli/v2/$VERSION/dist/awscli/data/ac.index awscli/data/
