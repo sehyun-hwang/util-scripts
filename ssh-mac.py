83a1d3eb6c3639b13a15749e2936e71752d0ce1e
Sehyun Hwang <hwanghyun3@gmail.com>
2024-04-28 22:28:51 +0900

    ssh-like arg parse in ssh-mac.py

--- ssh-mac.py
+++ ssh-mac.py
@@ -1,28 +1,34 @@
+import argparse
 import logging
+from contextlib import ExitStack
 from pathlib import Path
 from time import sleep
-import argparse
 
 from paramiko import MissingHostKeyPolicy, SSHClient
 from paramiko.ssh_exception import SSHException
+from paramiko_tunnel.tunnel import Tunnel
 from remoteit_ssh_client import main
 
 from interactive_shell import open_shell
-from paramiko_tunnel.tunnel import Tunnel
 
 logging.basicConfig()
-# logging.getLogger("paramiko").setLevel(logging.DEBUG)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="SSH Argument Parser")
-    parser.add_argument("-J", dest='jump_host')
-    parser.add_argument("-L", dest='tunnel', nargs='+', metavar="port:host:hostport")
+    parser.add_argument("-J", dest="jump_host")
+    parser.add_argument("-N")
+    parser.add_argument("-L", dest="tunnels", nargs="+", metavar="port:host:hostport")
+    parser.add_argument("-v", dest="verbose", action="store_true")
     parser.add_argument("host")
 
     args = parser.parse_args()
     print(args)
+
+    if args.verbose:
+        logging.getLogger("paramiko").setLevel(logging.DEBUG)
     return args
+
 
 def init_instance():
     client = SSHClient()
@@ -36,23 +42,40 @@ def init_instance():
                 *connection_params.values(),
                 timeout=1,
                 username="hwangsehyun",
-                key_filename=str(Path("~/.ssh/id_ed25519").expanduser())
+                key_filename=str(Path("~/.ssh/id_ed25519").expanduser()),
             )
             return client
-        except SSHException:
+        except SSHException as error:
             logging.exception("ssh exception")
 
 
-if __name__ == "__main__":
-    parse_arguments()
-    #asdf
-    client = init_instance()
-    with Tunnel(
-        paramiko_session=client,
-        remote_host='localhost',
-        remote_port=8889,
-    ) as tunnel:
-        print(f'{tunnel.bind_address=} {tunnel.bind_port=}')
+def execute_tunnel(args):
+    with ExitStack() as stack:
+        for tunnel_arg in args.tunnels:
+            *local, remote_host, remote_port = tunnel_arg.split(":")
+            kwargs = {}
+            if len(local) == 1:
+                kwargs["bind_address_and_port"] = ("", int(local[0]))
+            elif len(local) == 2:
+                kwargs["bind_address_and_port"] = (local[0], int(local[1]))
+
+            tunnel = stack.enter_context(
+                Tunnel(
+                    paramiko_session=client,
+                    remote_host="localhost",
+                    remote_port=int(remote_port),
+                    **kwargs,
+                ),
+            )
+            print(tunnel)
         while True:
             sleep(1)
-    open_shell(client)
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    client = init_instance()
+    if args.tunnels:
+        execute_tunnel(args)
+    else:
+        open_shell(client)
