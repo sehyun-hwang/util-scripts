069b7215dfc994717d8d152596cb50785078ae5c
Sehyun Hwang <hwanghyun3@gmail.com>
2024-09-23 14:57:47 +0900

    ATUIN_SYNC_ADDRESS in OrbStack

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
 set BREW_EXECUTABLE (ls /home/linuxbrew/.linuxbrew/bin/brew /opt/homebrew/bin/brew 2> /dev/null)
 
 if [ "$BREW_EXECUTABLE" ]
+    eval ($BREW_EXECUTABLE shellenv)
+
     # https://docs.brew.sh/Shell-Completion
     if test -d (brew --prefix)"/share/fish/completions"
         set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
@@ -22,9 +24,13 @@ end
 # pnpm end
 
 atuin init fish | .
-export ATUIN_SYNC_ADDRESS=(docker ps -f name=atuin --format '{{.Ports}}' | sed -n 's=.*:\([0-9]*\)->.*=http://localhost:\1=p')
+if test (uname) = Darwin
+    export ATUIN_SYNC_ADDRESS=http://atuin.orb.local:8888
+else
+    export ATUIN_SYNC_ADDRESS=(docker ps -f name=atuin --format '{{.Ports}}' | sed -n 's=.*:\([0-9]*\)->.*=http://localhost:\1=p')
+end
 
-if not test -S "(realpath /var/run/docker.sock)"
+if not test -w (realpath /var/run/docker.sock)
     export DOCKER_HOST=unix:///run/user/(id -u)/podman/podman.sock
 end
 
